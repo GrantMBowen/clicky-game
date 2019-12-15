78e0385fed4a6c058feece4066a6186ef6c0b1b8
itsgoodtobegrant <itsgoodtobegrant@gmail.com>
2019-12-14 19:43:13 -0800

    Updates

---
+++ precache-manifest.b08aa5607dae44257ad778353fd5090e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e5d2597ef746fa272ff54ca6003665",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "8f8a4363c606d7b6cfc8",
+    "url": "/clicky-game/static/css/main.eef6c380.chunk.css"
+  },
+  {
+    "revision": "00d61d0bbab4af0d0cec",
+    "url": "/clicky-game/static/js/2.59c7650b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.59c7650b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f8a4363c606d7b6cfc8",
+    "url": "/clicky-game/static/js/main.6c91b459.chunk.js"
+  },
+  {
+    "revision": "b859e8c835d6a54c0ef8",
+    "url": "/clicky-game/static/js/runtime-main.bf081868.js"
+  }
+]);
